b15b19e24dbdf6af44d0a1e89e27a01e58230582
Zappp <marcinkomar97@gmail.com>
2020-05-16 00:42:46 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,8 @@ using mopkg, Test
 
 @testset "mopkg" begin
     x, e, f = 10, 0.001, x -> x^4 + 3x^3 + x^2 + sin(x)
-    @test inpol_dsc(f, x, e) == (-1.9712756211432556, -4.915216432826824)
+    @test inpol_dsc(f, x, e) == (-1.9712756210476652, -4.915216432826824)
+    @test inpol_dsc(f, x, e) == (0, 0)
+
 end
 
