edef9baedd6c24878697122f5220cc5d0d75b658
Zappp <marcinkomar97@gmail.com>
2020-05-15 23:51:57 +0200

    .

--- src/mopkg.jl
+++ src/mopkg.jl
@@ -1,6 +1,61 @@
-module mopkg
-my_func(x) = x^2
+# x0k -> starting point
+# e -> epsilon
+# s -> sigma
 
-export my_func
+module interpolation_DSC
 
+
+function rec_x(f, xp1k, s, p, n=1, xp2k=nothing)
+    xnk = xp1k + (2 ^ (n - 1) * p * s)
+    if f(xnk) > f(xp1k)
+        return n, xp1k, xnk, xp2k
+    end
+
+    return rec_x(f, xnk, s, p, n + 1, xp1k)
+end
+
+function inpol_dsc(f, x0k, e, s=0.1, K=0.01)
+    xp1k = x0k - s
+    xn1k = x0k + s
+    f0k = f(x0k)
+    fn1k = f(xn1k)
+    if f0k > fn1k
+        p = 1
+        n, xp1k, xnk, xp2k = rec_x(f, x0k, s, p)
+
+    else
+        fp1k = f(xp1k)
+        if fp1k < f0k
+            p = -1
+            n, xp1k, xnk, xp2k = rec_x(f, x0k, s, p)
+        
+        elseif fp1k >= f0k && f0k <= fn1k
+            x0kn1 = x0k + ((s * (fp1k - fn1k)) / (2 * (fp1k - (2 * f0k) + fn1k)))
+
+            if s <= e
+                return x0kn1, f(x0kn1)
+            else
+                inpol_dsc(f, x0kn1,e ,s * K)
+            end
+        end
+    end
+
+    xmk = xp1k + (2 ^ (n - 2) * p * s)
+    fmk = f(xmk)
+
+    if fmk >= f(xp1k)
+        x0kn1 = xp1k + ((2 ^ (n - 2) * p * s * (f(xp2k) - fmk)) / (2 * (
+                f(xp2k) - 2 * f(xp1k) + fmk)))
+    elseif fmk < f(xp1k)
+        x0kn1 = xmk + (((2 ^ (n - 2)) * p * s * (f(xp1k) - f(xnk))) / (2 * (f(xp1k) - (2 * fmk) + f(xnk))))
+    end
+
+    if (2 ^ (n - 2)) * s <= e
+        return x0kn1, f(x0kn1)
+    else
+        inpol_dsc(f, x0kn1, e,s * K)
+    end
+end
+
+export inpol_dsc
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
-using mopkg
-using Test
+using interpolation_DSC, Test
 
-@testset "mopkg.jl" begin
-@test my_func(2) == 4
+@testset "interpolation_DSC" begin
+    x, e, f = 10, 0.001, x -> x^4 + 3x^3 + x^2 + sin(x)
+    @test inpol_dsc(f, x, e) == (-1.9712756211432556, -4.915216432826824)
+    @test inpol_dsc(f, x, e) == (-8, -4)
 end
+
